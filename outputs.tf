ab0717b3f28f001c951fc7d8fd9aed97ae46aeb5
John Adedigba <john.adedigba@digitalturbine.com>
2025-11-06 11:58:06 +0100

    fix: update outputs to match new module structure
    - Changed from john_iam_user to iam_users module
    - Output all users instead of just one
    - Added SSM parameter paths for password retrieval
    - Marked passwords as sensitive

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,15 @@
-output "john_iam_user_name" {
-  description = "The name of the created IAM user"
-  value       = module.john_iam_user.user_name
+output "iam_user_names" {
+  description = "Names of all created IAM users"
+  value       = module.iam_users.user_names
 }
 
-output "john_temp_password_ssm_path" {
-  description = "The SSM path where the temporary password is stored"
-  value       = "/iam/${var.user_name}/temp_password"
+output "iam_user_temp_passwords" {
+  description = "Temporary passwords for all created users (retrieve from SSM Parameter Store)"
+  value       = module.iam_users.user_temp_passwords
+  sensitive   = true
+}
+
+output "ssm_parameter_paths" {
+  description = "SSM Parameter Store paths where passwords are stored"
+  value       = [for user in module.iam_users.user_names : "/iam/${user}/temp_password"]
 }
