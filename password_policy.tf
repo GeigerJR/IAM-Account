1155a42ae778a860dd7fde57393675c51422dab6
John Adedigba <john.adedigba@digitalturbine.com>
2025-11-06 11:59:23 +0100

    refactor: move password policy to root level
    - Password policy is account-wide, not module-specific
    - Moved from modules/iam_user/ to root directory
    - Added password_reuse_prevention setting
    - Only one password policy allowed per AWS account

--- password_policy.tf
+++ password_policy.tf
@@ -1,3 +1,6 @@
+# AWS IAM Account Password Policy
+# This is an account-wide setting - only one policy per AWS account
+
 resource "aws_iam_account_password_policy" "secure_policy" {
   minimum_password_length        = 14
   require_lowercase_characters   = true
@@ -6,4 +9,6 @@ resource "aws_iam_account_password_policy" "secure_policy" {
   require_symbols                = true
   allow_users_to_change_password = true
   max_password_age               = 90
+  password_reuse_prevention      = 5
 }
+
